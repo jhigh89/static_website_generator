2d740fdcf346cd81500b0ab4b73eb43598a60b5c
jhigh89 <jhightower928@gmail.com>
2024-12-09 23:08:21 -0800

    initial commit of files: main.py,src dir, pub dir

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode,TextType
+
+def main():
+    test_textnode = TextNode("It's me, the text node!",TextType.TEXT)
+    print(test_textnode)
+
+if __name__ == main():
+    main()
