d5df7d34b72b0ddc34b749945a2e803a7ccf75c5
jhigh89 <jhightower928@gmail.com>
2024-12-15 00:11:44 -0800

    added a lot, basically finished the project but didn't put a lot of unit tests in. i really just wanted to move past this one and get on to more interesting ones.

---
+++ src/block_markdown.py
@@ -0,0 +1,155 @@
+from htmlnode import ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
+
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
+def markdown_to_blocks(markdown):
+    return list(filter(None,list(map(lambda x: x.strip(), markdown.split("\n\n")))))
+
+def is_heading(block: str) -> bool:
+    # Check if the first line starts with a Markdown heading indicator
+    return block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### "))
+
+def is_code_block(lines: list[str]) -> bool:
+    # Code blocks are indicated by triple backticks on the first and last lines
+    return len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```")
+
+def is_all_quoted(lines: list[str]) -> bool:
+    # All lines must start with '>' for a quote block
+    return all(line.startswith(">") for line in lines)
+
+def is_unordered_list(lines: list[str]) -> bool:
+    # Check if all lines start with '* ' or all start with '- '
+    if all(line.startswith("* ") for line in lines):
+        return True
+    if all(line.startswith("- ") for line in lines):
+        return True
+    return False
+
+def is_ordered_list(lines: list[str]) -> bool:
+    # Check if each line starts with "1. ", "2. ", "3. ", etc.
+    for i, line in enumerate(lines, start=1):
+        expected_prefix = f"{i}. "
+        if not line.startswith(expected_prefix):
+            return False
+    return True
+
+def block_to_block_type(block: str) -> str:
+    lines = block.split("\n")
+
+    if is_heading(block):
+        return block_type_heading
+    if is_code_block(lines):
+        return block_type_code
+    if block.startswith(">"):
+        return block_type_quote if is_all_quoted(lines) else block_type_paragraph
+    if block.startswith("* ") or block.startswith("- "):
+        return block_type_ulist if is_unordered_list(lines) else block_type_paragraph
+    if block.startswith("1. "):
+        return block_type_olist if is_ordered_list(lines) else block_type_paragraph
+
+    return block_type_paragraph
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
+
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    if block_type == block_type_paragraph:
+        return paragraph_to_html_node(block)
+    if block_type == block_type_heading:
+        return heading_to_html_node(block)
+    if block_type == block_type_code:
+        return code_to_html_node(block)
+    if block_type == block_type_olist:
+        return olist_to_html_node(block)
+    if block_type == block_type_ulist:
+        return ulist_to_html_node(block)
+    if block_type == block_type_quote:
+        return quote_to_html_node(block)
+    raise ValueError("Invalid block type")
+
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+
+def heading_to_html_node(block):
+    level = 0
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"Invalid heading level: {level}")
+    text = block[level + 1 :]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
+
+
+def code_to_html_node(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("Invalid code block")
+    text = block[4:-3]
+    children = text_to_children(text)
+    code = ParentNode("code", children)
+    return ParentNode("pre", [code])
+
+
+def olist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
+
+
+def ulist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("Invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+
--- src/main.py
+++ src/main.py
@@ -1,16 +1,44 @@
 from textnode import TextNode,TextType
 from htmlnode import HTMLNode,LeafNode,ParentNode
 from inline_markdown import text_to_textnodes
+from gencontent import generate_pages_recursive
+import os,shutil
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
-    test_textnode = TextNode("It's me, the text node!",TextType.TEXT)
-    test_htmlnode = HTMLNode(props={
-    "href": "https://www.google.com", 
-    "target": "_blank",
-    })
+    print("Deleting public directory contents...")
+    clean_directory(dir_path_public)
+    print("Copying static files to public directory...")
+    move_files_to_public_directory(dir_path_static,dir_path_public)
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
-    text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-    print(text_to_textnodes(text))
+def clean_directory(directory):
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        if os.path.isfile(filepath):
+            os.remove(filepath)
+        elif os.path.isdir(filepath):
+            shutil.rmtree(filepath)
+
+def move_files_to_public_directory(source_directory, destination_directory):
+    for item in os.listdir(source_directory):
+        s = os.path.join(source_directory, item)
+        d = os.path.join(destination_directory, item)
+        
+        if os.path.isdir(s):
+            # If the directory already exists at the destination, remove it
+            if os.path.exists(d):
+                shutil.rmtree(d)
+            # Recursively copy the entire directory tree
+            shutil.copytree(s, d)
+        else:
+            # Copy a single file, preserving metadata
+            shutil.copy2(s, d)
 
 if __name__ == "__main__":
     main()
